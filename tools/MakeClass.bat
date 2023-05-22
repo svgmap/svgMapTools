138a370d8bb9997204715968e6ca4a7cd2196d48
Satoru Takagi <satakagi@gmail.com>
2023-05-22 18:46:09 +0900

    fix bug MakeClass.bat

--- tools/MakeClass.bat
+++ tools/MakeClass.bat
@@ -3,6 +3,9 @@ rmdir /s /q src_cpy
 
 xcopy /e src src_cpy\
 
+xcopy /e src_cpy\main\resources src_cpy\main\java
+
+
 cd src_cpy\main\java
 
 set CP=%CLASSPATH%;..\..\..\target\dependency\*;.
@@ -10,6 +13,6 @@ set CP=%CLASSPATH%;..\..\..\target\dependency\*;.
 javac -encoding utf-8 -classpath %CP% org\svgmap\shape2svgmap\svgMapMarkFactory.java
 javac -encoding utf-8 -classpath %CP% org\svgmap\shape2svgmap\MainWrapper.java
 
-jar cf ..\..\..\target\svgMapTools-202305.jar ..\resources\META-INF\services\* org\svgmap\shape2svgmap\*.class org\svgmap\shape2svgmap\cds\*.class
+jar cf ..\..\..\target\svgMapTools-202305.jar META-INF\services\* org\svgmap\shape2svgmap\*.class org\svgmap\shape2svgmap\cds\*.class
 
 cd ..\..\..\tools
